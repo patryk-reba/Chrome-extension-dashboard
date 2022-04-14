2234a758a7c4960a51492b0eaef2d8a66b327c25
Patryk <patrykreba95@gmail.com>
2022-04-14 15:38:02 +0200

    update

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
+        document.getElementById("author").textContent = `By: ${data.user.name}`
     })
     .catch(err => {
         // Use a default background image/author
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
 )`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
+        document.getElementById("author").textContent = `By: Dodi Achmad`
     })
 
 fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
@@ -33,7 +33,7 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
 
 function getCurrentTime() {
     const date = new Date()
-    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
+    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", { timeStyle: "short" })
 }
 
 setInterval(getCurrentTime, 1000)
